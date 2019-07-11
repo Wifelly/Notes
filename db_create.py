f9e2dd70814ff205f3babcefcaf64d233531bd15
Wifelly <supermegawifi@gmail.com>
2019-07-11 15:54:53 +0500

    even more bugfixes

--- db_create.py
+++ db_create.py
@@ -7,7 +7,7 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS Notes (
                     id TEXT UUID PRIMARY KEY UNIQUE NOT NULL,
                     title TEXT NOT NULL,
                     text TEXT,
-                    “date_create” INTEGER NOT NULL,
-                    “date_update” INTEGER NOT NULL
+                    date_create INTEGER NOT NULL,
+                    date_update INTEGER NOT NULL
                     );
                """)
