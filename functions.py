5d715535f2f28a4c2084c3d1e787b2dc0ed00579
Wifelly <supermegawifi@gmail.com>
2019-07-11 15:41:04 +0500

    POST semi-finished

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import sqlite3
+import uuid
 
 from flask import Flask, request, Response, redirect, jsonify
 
@@ -8,6 +9,9 @@ db = request_db('db.db')
 
 
 def add_note(data):
+    if ('title' not in data) or ('text' not in data):
+        return jsonify({"msg": "Bad request"}), 400
+    
     return jsonify({"msg": "Not implemented yet"}), 501
 
 
